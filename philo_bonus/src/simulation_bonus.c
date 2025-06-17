ff7db2962ef517e8c5beeb93195beb3a0a0682e2
Amine Elsayed <aelsayed@c4r8p10.1337.ma>
2025-06-18 00:11:16 +0100

    some garbage work

--- philo_bonus/src/simulation_bonus.c
+++ philo_bonus/src/simulation_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   simulation_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sayed <sayed@student.42.fr>                +#+  +:+       +#+        */
+/*   By: aelsayed <aelsayed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 09:59:40 by sayed             #+#    #+#             */
-/*   Updated: 2025/06/17 18:06:21 by sayed            ###   ########.fr       */
+/*   Updated: 2025/06/18 00:00:13 by aelsayed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,30 @@ long	get_time(void)
 	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
 }
 
+void	kill_all_philosophers(t_table *t)
+{
+	int i = 0;
+	while (i < t->nb_philo)
+	{
+		kill(t->philos[i].pid, SIGKILL);
+		waitpid(t->philos[i].pid, NULL, 0);
+		i++;
+	}
+}
 void	monitor(t_table *t)
 {
 	int		status;
-	int		i;
 	pid_t	pid;
 
-	// just to wait to any child i do -1
-	pid = waitpid(-1, &status, 0);
-	i = -1;
-	if (pid > 0 && WIFEXITED(status) && WEXITSTATUS(status) == 1)
-		while (++i < t->nb_philo)
-			if (t->philos[i].pid != pid)
-				kill(t->philos[i].pid, SIGKILL);
-	i = 0;
-	while (i < t->nb_philo)
-		waitpid(t->philos[i++].pid, NULL, 0);
+	while ((pid = waitpid(-1, &status, 0)) > 0)
+	{
+		if (WIFEXITED(status) && WEXITSTATUS(status) == 1)
+		{
+			kill_all_philosophers(t);
+			break ;
+		}
+	}
+	kill_all_philosophers(t);
 	sem_close(t->forks);
 	sem_close(t->print);
 	sem_close(t->death);
@@ -58,40 +66,31 @@ void	routine(t_philo *p)
 			continue ;
 		if (process_thinking(p) == FALSE)
 			continue ;
-		usleep(1000);
+		usleep(500);
 	}
 	exit(1);
 }
 
 void	check_death_and_full(t_philo *p, long now, int full)
 {
+	(void)full;
 	sem_wait(p->t->meal);
 	if (now - p->last_meal_time >= p->t->time_to_die)
 	{
+		sem_post(p->t->meal);
 		sem_wait(p->t->death);
 		p->t->someone_died = TRUE;
 		sem_post(p->t->death);
 		sem_wait(p->t->print);
 		printf("%ld %d died\n", get_time() - \
-		p->t->start_time, p->t->philos[p->t->i].id);
+		p->t->start_time, p->id);
 		sem_post(p->t->print);
-		sem_post(p->t->meal);
 		exit(1);
 	}
-	if (p->t->meals_required != -1)
+	if (p->t->meals_required != -1 && p->meals_eaten >= p->t->meals_required)
 	{
-		p->t->i = -1;
-		while (++p->t->i < p->t->nb_philo)
-			if (p->t->philos[p->t->i].meals_eaten >= \
-				p->t->meals_required)
-				full++;
-		if (full == p->t->nb_philo)
-		{
-			sem_post(p->t->meal);
-			sem_wait(p->t->death);
-			p->t->all_full = p->t->nb_philo;
-			return (sem_post(p->t->death), exit(0));
-		}
+		sem_post(p->t->meal);
+		exit(0);
 	}
 	sem_post(p->t->meal);
 }
@@ -136,5 +135,5 @@ int simulation(t_table *t)
 		t->i++;
 	}
 	monitor(t);
-	return (TRUE);
+	return (TRUE);	
 }
